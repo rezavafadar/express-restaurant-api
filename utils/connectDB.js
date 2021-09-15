1fc8391584a218e31e0f6e44581f5bfa658c8749
reza vafadar <mrvafadar4@gmail.com>
2021-09-15 15:40:38 +0430

    add connect to db function

---
+++ utils/connectDB.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const uri =
+  process.env.APPMODE == "development"
+    ? process.env.DEVELOP_DB_URI
+    : process.env.PRODUCTION_DB_URI;
+
+module.exports = async () => {
+  try {
+    const conn = await mongoose.connect(uri, {
+      useCreateIndex: true,
+      useFindAndModify: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`connected to db ${conn.connection.host}..`);
+    console.log(conn);
+  } catch (err) {
+      console.log(err);
+      process.exit(1)
+  }
+};
