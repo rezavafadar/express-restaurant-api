bbd6a0c2d2ceb2b71cb45edff30f411be3ef6117
reza vafadar <mrvafadar4@gmail.com>
2021-09-29 18:12:03 +0330

    create and add food routes

---
+++ routes/food.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/:id')
+
+// authenticate middleware
+
+router.post('/add-food')
+
+router.patch('/:id')
+
+router.delete('/:id')
+module.exports = router
