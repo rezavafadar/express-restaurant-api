c5a6e6a9918c5ab1bd4096cf010c29c2f279eadd
reza vafadar <mrvafadar4@gmail.com>
2021-10-01 14:51:11 +0330

    change restaurant validation schema

--- model/secure/restaurant.js
+++ model/secure/restaurant.js
@@ -2,9 +2,12 @@ const yup = require('yup');
 
 const validateSchema = yup.object().shape({
     name:yup.string().required('name is required'),
-    bio:yup.string().required('bio is required').min(8,'bio must not be less than 8 characters'),
+    description:yup.string().required('bio is required').min(8,'description must not be less than 8 characters'),
     phone:yup.string().required('phone is required'),
-    address:yup.string().required('address is required')
+    address:yup.string().required('address is required'),
+    username:yup.string().required('username is required').min(4,'username must not be less than 8 characters'),
+    password:yup.string().required('password is required').min(6,'password must not be less than 8 characters'),
+    confirmPassword:yup.string().required('confirm Password is required').oneOf([yup.ref('password'),null])
 }) 
 
 module.exports = validateSchema
